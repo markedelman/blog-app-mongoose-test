8ebaf88c1bc54b4b20257402244bd88387e85214
Mark Edelman <markedelman1987@gmail.com>
2016-12-22 17:31:10 -0500

    seeding function and generator function started in js file

--- test/blog-app-mongoose-test.js
+++ test/blog-app-mongoose-test.js
@@ -4,9 +4,45 @@ const faker = require('faker');
 const mongoose = require('mongoose');
 
 
+
 const should = chai.should();
 
-const {Restaurants} = require ('../models');
+const {BlogPost} = require ('../models');
 const {app, runServer, closeServer} = require('../server');
 
+
 chai.use(chaiHttp);
+
+
+
+
+// our data seeding function - step 1 of 4?
+
+function seedBlogPostData() {
+  console.info('seeding blog post data');
+  const seedData = [];
+
+  for (let i=1; i<=10; i++) {
+    seedData.push(generateBlogPostData());
+  }
+  // return a seeding promise
+  return BlogPost.insertMany(seedData);
+}
+
+function generateBlogPostData() {
+  return {
+    title: faker.company.catchPhrase(),
+    author: {firstName: faker.name.firstName(),
+             lastName: faker.name.lastName()},
+    content: faker.lorem.paragraphs()
+  }
+}
+
+
+// {
+// "title": "20 things -- you won't believe #4",
+// "author": {"firstName": "Tabernacle", "lastName": "Jeff"},
+// "content": "Lorem ....."
+// }
+
+//faker.company.catchPhrase
